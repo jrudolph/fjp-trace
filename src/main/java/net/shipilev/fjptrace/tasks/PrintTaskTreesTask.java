15158004f9b8eff873d85f84831348e91f9809f2
Aleksey Shipilev <aleksey.shipilev@gmail.com>
2012-10-16 15:51:05 +0400

    Emit basic subtree chart.

--- src/main/java/net/shipilev/fjptrace/tasks/PrintTaskTreesTask.java
+++ src/main/java/net/shipilev/fjptrace/tasks/PrintTaskTreesTask.java
@@ -17,19 +17,30 @@
 package net.shipilev.fjptrace.tasks;
 
 import net.shipilev.fjptrace.Event;
+import net.shipilev.fjptrace.EventType;
 import net.shipilev.fjptrace.Events;
 import net.shipilev.fjptrace.Options;
 import net.shipilev.fjptrace.Task;
 import net.shipilev.fjptrace.TaskStatus;
+import net.shipilev.fjptrace.util.Multimap;
+import net.shipilev.fjptrace.util.PairedList;
 import org.apache.commons.math.stat.descriptive.DescriptiveStatistics;
+import sun.font.Type1Font;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.SortedMap;
 import java.util.TreeMap;
@@ -37,16 +48,27 @@ import java.util.TreeMap;
 public class PrintTaskTreesTask extends LoggedRecursiveAction {
 
     private final TaskStatus subgraphs;
-    private final String fileName;
+    private final String fileNameDot;
+    private final String fileNamePng;
     private final Events events;
     private final long fromTime;
     private final long toTime;
+    private final int width;
+    private final int height;
+
+    // transient
+    private long minTime;
+    private long maxTime;
+    private Map<Long,Integer> workerId;
 
     public PrintTaskTreesTask(Options opts, Events events, TaskStatus subgraphs) {
         super("Print task subtrees");
-        this.fileName = opts.getTargetPrefix() + "-subtrees.dot";
+        this.fileNameDot = opts.getTargetPrefix() + "-subtrees.dot";
+        this.fileNamePng = opts.getTargetPrefix() + "-subtrees.png";
         this.fromTime = opts.getFromTime();
         this.toTime = opts.getToTime();
+        this.width = opts.getWidth();
+        this.height = opts.getHeight();
         this.events = events;
         this.subgraphs = subgraphs;
     }
@@ -55,7 +77,7 @@ public class PrintTaskTreesTask extends LoggedRecursiveAction {
     public void doWork() throws Exception {
         // walk the trees
 
-        PrintWriter pw = new PrintWriter(fileName);
+        PrintWriter pw = new PrintWriter(fileNameDot);
 
         // only record the events for the interesting region
         for (Event e : events) {
@@ -80,7 +102,6 @@ public class PrintTaskTreesTask extends LoggedRecursiveAction {
             List<Task> prev = new ArrayList<>();
             List<Task> cur = new ArrayList<>();
 
-            List<Long> workers = new ArrayList<>();
             List<Event> events = new ArrayList<>();
 
             cur.add(t);
@@ -95,7 +116,6 @@ public class PrintTaskTreesTask extends LoggedRecursiveAction {
                         cur.addAll(children);
                     }
                     events.addAll(c.getEvents());
-                    workers.add(c.getWorker());
                 }
 
             }
@@ -109,27 +129,137 @@ public class PrintTaskTreesTask extends LoggedRecursiveAction {
                 }
             });
 
-            pw.println("digraph {");
-
-            // emit all sequencing for workers first
-            for (Long w : workers) {
-
-                Event lastEvent = null;
+            // enumerate workers
+            Set<Long> workers = new HashSet<>();
+            workerId = new HashMap<>();
+            {
+                int id = 0;
                 for (Event e : events) {
-                    if (e.workerId == w) {
-                        if (lastEvent != null) {
-                            pw.println(" " + lastEvent.shortID() + " -> " + e.shortID() + " [timeDiff = " + (e.time - lastEvent.time) + "];");
-                        }
-                        lastEvent = e;
+                    if (workers.add(e.workerId)) {
+                        workerId.put(e.workerId, id++);
                     }
                 }
             }
 
-            pw.println("}");
+            // figure out min and max time
+            minTime = Long.MAX_VALUE;
+            maxTime = Long.MIN_VALUE;
+            for (Event e : events) {
+                minTime = Math.min(minTime, e.time);
+                maxTime = Math.max(maxTime, e.time);
+            }
+
+            render(pw, events);
         }
 
         pw.flush();
         pw.close();
+    }
+
+    private Point map(Event e) {
+        return new Point(
+                (width * workerId.get(e.workerId) / workerId.size()),
+                (int)(height * (e.time - minTime) / (maxTime - minTime))
+                );
+    }
+
+    private void render(PrintWriter pw, Collection<Event> events) throws IOException {
+
+
+        // split tasks
+        Multimap<Integer, Event> tasks = new Multimap<>();
+        for (Event e : events) {
+            tasks.put(e.taskTag, e);
+        }
+
+        // figure out the edges
+
+        List<Pair<Event, Event>> edges = new ArrayList<>();
+
+        // Intra-thread edges
+        for (Long w : workerId.keySet()) {
+            Event lastEvent = null;
+            for (Event e : events) {
+                if (w == e.workerId) {
+                    if (lastEvent != null) {
+                        edges.add(new Pair<>(lastEvent, e));
+                    }
+                    lastEvent = e;
+                }
+            }
+        }
+
+        // Inter-thread edges:
+        //   SUBMIT -> EXEC
+        //     FORK -> EXEC
+        //     EXECED -> JOINED
+        for (Integer id : tasks.keySet()) {
+            List<Event> list = tasks.get(id);
+
+            Multimap<EventType, Event> byType = new Multimap<>();
+            for (Event e : list) {
+                byType.put(e.eventType, e);
+            }
+
+            edges.addAll(product(byType.get(EventType.SUBMIT), byType.get(EventType.EXEC)));
+            edges.addAll(product(byType.get(EventType.FORK), byType.get(EventType.EXEC)));
+            edges.addAll(product(byType.get(EventType.EXECUTED), byType.get(EventType.JOINED)));
+        }
+
+        // output .dot
+        pw.println("digraph {");
+        for (Pair<Event, Event> pair : edges) {
+            pw.println(" " + pair.t1.shortID() + " -> " + pair.t2.shortID() + " [timeDiff = " + (pair.t2.time - pair.t1.time) + "];");
+        }
+        pw.println("}");
+
+        // render graph: prepare canvas
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+        Graphics2D g = image.createGraphics();
+        g.setBackground(Color.WHITE);
+
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, width, height);
+
+
+        // render graph: nodes
+        for (Event e : events) {
+            Point p = map(e);
+            g.setColor(Color.BLACK);
+            g.fillRect(p.x, p.y, 3, 3);
+        }
+
+        // render graph: edges
+        for (Pair<Event, Event> pair : edges) {
+            Point p1 = map(pair.t1);
+            Point p2 = map(pair.t2);
+
+            g.setColor(Color.GREEN);
+            g.drawLine(p1.x, p1.y, p2.x, p2.y);
+        }
+
+        ImageIO.write(image, "png", new File(fileNamePng));
+    }
+
+    private <T1, T2> Collection<Pair<T1, T2>> product(Collection<T1> list1, Collection<T2> list2) {
+        List<Pair<T1, T2>> result = new ArrayList<Pair<T1, T2>>();
+        for (T1 e1 : list1) {
+            for (T2 e2 : list2) {
+                result.add(new Pair<T1, T2>(e1, e2));
+            }
+        }
+        return result;
+    }
+
+    private static class Pair<T1, T2> {
+        private final T1 t1;
+        private final T2 t2;
+
+        public Pair(T1 t1, T2 t2) {
+            this.t1 = t1;
+            this.t2 = t2;
+        }
     }
 
 }
